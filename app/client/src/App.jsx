b6f32fab96188f339729c45a255c12f341723bfc
Shreyansh Kumar <your-email@example.com>
2025-01-25 22:29:52 +0530

    Auth pages UI and evm wallet integration

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,68 +1,106 @@
-import { BrowserRouter , Route, Routes } from "react-router-dom"
-import Register from './page/register/index.jsx'
-import { useContext} from 'react';
-import Web3 from 'web3';
-import Wallet from './components/wallet';
-import { Context } from './context/context';
+import { useState } from "react";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./page/register/index.jsx";
+import { useContext } from "react";
+import { Context } from "./context/context";
 import SignIn from "./page/signin/index.jsx";
 import Dashboard from "./page/dashboard/index.jsx";
 import P2P from "./page/p2p/index.jsx";
+import KYC from "./page/kyc/index.jsx";
+import styles from "./styles/Home.module.css";
+
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import { arbitrum, mainnet } from "viem/chains";
+
+// 1. Get projectId
+const projectId = "556f704d7102922b2bfd2c6b88c70969";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "web3-modal-setup",
+  description: "Web3 Modal Example",
+};
+
+const chains = [mainnet, arbitrum];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains });
 
 function App() {
+  const [isNetworkSwitchHighlighted, setIsNetworkSwitchHighlighted] =
+    useState(false);
+  const [isConnectHighlighted, setIsConnectHighlighted] = useState(false);
 
-  const context = useContext(Context)
-  const {setAccData , isConnected , setIsConnected} = context
-
-  const detectCurrentProvider = () => {
-    let provider;
-    if (window.ethereum) {
-      provider = window.ethereum;
-    } else if (window.web3) {
-      provider = window.web3.currentProvider;
-    } else {
-      alert("Window does not support MEtamask")
-    }
-    return provider;
+  const closeAll = () => {
+    setIsNetworkSwitchHighlighted(false);
+    setIsConnectHighlighted(false);
   };
-  
-  const onConnect = async() => {
-    try {
-      const currentProvider = detectCurrentProvider();
-      if(currentProvider) {
-        await currentProvider.request({method: 'eth_requestAccounts'});
-        const web3 = new Web3(currentProvider);
-        const userAccount  =await web3.eth.getAccounts();
-        let ethAddress = userAccount[0];
-        let ethBalance = await web3.eth.getBalance(ethAddress);
-        ethBalance = web3.utils.fromWei(ethBalance, 'ether'); 
-        const data = {
-          balance:ethBalance, address:ethAddress
-        }
-        setAccData(data)
-        setIsConnected(true);
-      }
-    } catch(err) {
-      console.log(err);
-    }
-  }
+
+  const context = useContext(Context);
+  const { setAccData, isConnected, setIsConnected } = context;
+
+  const [formData, setFormData] = useState({
+    tokenId: 0,
+    user: "",
+  });
+
+  const [key, setKey] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
 
   return (
     <>
-      <div style={{display:'fixed'}}>
-        {
-          !isConnected && <Wallet onConnect={onConnect}/>
-        }
+      <div className="wallets">
+        <WagmiConfig config={wagmiConfig}>
+          <header>
+            <div
+              className={styles.backdrop}
+              style={{
+                opacity:
+                  isConnectHighlighted || isNetworkSwitchHighlighted ? 1 : 0,
+              }}
+            />
+            <div className={styles.header}>
+              <div className={styles.buttons}>
+                <div
+                  onClick={closeAll}
+                  className={`${styles.highlight} ${
+                    isNetworkSwitchHighlighted ? styles.highlightSelected : ``
+                  }`}
+                >
+                  <w3m-network-button />
+                </div>
+                <div
+                  onClick={closeAll}
+                  className={`${styles.highlight} ${
+                    isConnectHighlighted ? styles.highlightSelected : ``
+                  }`}
+                >
+                  <w3m-button />
+                </div>
+              </div>
+            </div>
+          </header>
+        </WagmiConfig>
       </div>
       <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/p2p" element={<P2P/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/p2p" element={<P2P />} />
+          <Route path="/kyc" element={<KYC />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- app/client/src/page/signin/index.jsx
+++ app/client/src/page/signin/index.jsx
@@ -1,76 +1,112 @@
-import { useContext, useState } from "react"
+import { useContext, useState } from "react";
 import { SignInBusiness } from "../../apis/auth.contracts.js";
-import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { Context } from "../../context/context.jsx";
 import CryptoJS from "crypto-js";
+import logo from "../../assets/logo.png";
 
-const SignIn = ()=>{
+const SignIn = () => {
+  const navigate = useNavigate();
+  const context = useContext(Context);
+  const { setAccData } = context;
 
-    const navigate = useNavigate();
-    const context = useContext(Context);
-    const {setAccData} = context;
+  const [formData, setFormData] = useState({
+    tokenId: 0,
+    user: "",
+  });
 
-    const [formData , setFormData] = useState({
-        tokenId:0 , user:""
-    });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
 
-    const handleChange = (e)=>{
-        const { name, value } = e.target;
-        setFormData((prevFormData) => ({...prevFormData,[name]: value}));
-    }
+  const [key, setKey] = useState("");
 
-    const [key,setKey] = useState("")
+  function handleDecrypt(data) {
+    const bytes = CryptoJS.AES.decrypt(data, key);
+    const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    return decryptedData;
+  }
 
-    function handleDecrypt(data){
-        const bytes = CryptoJS.AES.decrypt(data,key);
-        const decryptedData= JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-        return decryptedData
-    }
-
-    async function handleSubmit() {
-        try {
-            if(!formData.tokenId) return;
-            const transaction = await SignInBusiness({formData});
-            if(!transaction) {
-                alert('Could not Sign-in');
-                return;
-            }
-            const cid = transaction.slice(7);
-            // const response = await axios.get(`https://ipfs.io/ipfs/${cid}`,{
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //     },
-            // });
-            const response = await axios.get(`http://localhost:3000/api/v1/company/signin/${cid}`,{
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            const dataa = handleDecrypt(response.data.data.data)
-            setAccData({...dataa , tokenId:formData.tokenId , key:key});
-            navigate('/dashboard');
-        } catch (error) {
-            alert(`some Error Occured\n${error}`);
-            console.log('Some Error Occured\n',error);
+  async function handleSubmit() {
+    try {
+      if (!formData.tokenId) return;
+      const transaction = await SignInBusiness({ formData });
+      if (!transaction) {
+        alert("Could not Sign-in");
+        return;
+      }
+      const cid = transaction.slice(7);
+      // const response = await axios.get(`https://ipfs.io/ipfs/${cid}`,{
+      //     headers: {
+      //         'Content-Type': 'application/json',
+      //     },
+      // });
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/company/signin/${cid}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
+      const dataa = handleDecrypt(response.data.data.data);
+      setAccData({ ...dataa, tokenId: formData.tokenId, key: key });
+      navigate("/dashboard");
+    } catch (error) {
+      alert(`some Error Occured\n${error}`);
+      console.log("Some Error Occured\n", error);
     }
+  }
 
-    return(
-        <>
-            <form style={{display:'flex',flexDirection:'column',width:'60vw'}}
-            onSubmit={(e)=>{
-                e.preventDefault();
-                handleSubmit();
-            }}>
-                <input type="text" name="tokenId" placeholder="Enter your Token Id" onChange={handleChange} required/>
-                <input type="text" name="user" placeholder="Enter your Wallet Address" onChange={handleChange} required/>
-                <input type="text" name="private_key" placeholder="Enter your Password" onChange={(e)=>{setKey(e.target.value)}} required/>
-                <input type="submit" value="SignIn"/>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <div className="col1">
+          <img src={logo} alt="" />
+        </div>
 
-}
+        <div className="col2">
+          <h1 className="login">Login to your Account</h1>
 
-export default SignIn
+          <form className="create">
+            <label>Enter your Token Id</label>
+            <input
+              type="text"
+              name="tokenId"
+              onChange={handleChange}
+              required
+            />
+            <label>Enter your Wallet Address</label>
+
+            <input type="text" name="user" onChange={handleChange} required />
+            <label>Enter your Password</label>
+            <input
+              type="text"
+              name="private_key"
+              onChange={(e) => {
+                setKey(e.target.value);
+              }}
+              required
+            />
+
+            <button className="submit">Proceed</button>
+          </form>
+          <p className="register">
+            Not A User?{" "}
+            <a
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              Register
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
